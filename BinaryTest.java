5249bfaa72be8fca95560532250a68c954694b88
chaos0103 <lyt1228@naver.com>
2021-12-29 22:29:10 +0900

    Binary

---
+++ BinaryTest.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class BinaryTest {
+    public static void main(String[] args){
+        int num = 10;
+        int bNum = 0B1010;  //0B: 2
+        int oNum = 012;     //0: 8
+        int hNum = 0XA;     //0X: 16
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(hNum);
+    }
+}
