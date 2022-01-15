e4e0938066debae7eb0c5c66cf3aca22bf5690e3
chaos0103 <lyt1228@naver.com>
2022-01-15 09:57:46 +0900

    CallAnotherConst

---
+++ chapter02/thisex/CallAnotherConst.java
@@ -0,0 +1,32 @@
+package Java.chapter02.thisex;
+
+class Person {
+    String name;
+    int age;
+
+    Person() {
+        // this를 사용해 Person(String, int) 생성자 호출
+        this("이름 없음", 1);
+    }
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person returnItSelf() {
+        return this;
+    }
+}
+
+public class CallAnotherConst {
+    public static void main(String[] args) {
+        Person noName = new Person();
+        System.out.println(noName.name);
+        System.out.println(noName.age);
+
+        Person p = noName.returnItSelf();
+        System.out.println(p);
+        System.out.println(noName);
+    }
+}
