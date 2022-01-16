123ab911a98c8d365d1ce7e30db494d6a056aefc
chaos0103 <lyt1228@naver.com>
2022-01-16 16:23:49 +0900

    ArrayListEx

---
+++ chapter03/arraylist/Subject.java
@@ -0,0 +1,23 @@
+package Java.chapter03.arraylist;
+
+public class Subject {
+    // Subject 클래스의 멤버 변수
+    private String name;  // 과목 이름
+    private int scorePoint;  // 과목 점수
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScorePoint() {
+        return scorePoint;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+}
